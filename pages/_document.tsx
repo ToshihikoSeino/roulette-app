8d62e9ddafb6ab0a9bec856dd432bf5a0288d506
Toshihiko Seino <seinonit@gmail.com>
2025-05-06 14:49:14 +0900

    不要なコード削除

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,11 +4,6 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <script
-          async
-          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1073151829758963"
-          crossOrigin="anonymous"
-        ></script>
       </Head>
       <body className="antialiased">
         <Main />
