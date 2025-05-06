eb98a8a4c79f91f055d067bf150a6daac74d7128
Toshihiko Seino <seinonit@gmail.com>
2025-05-06 12:57:39 +0900

    ok

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,13 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1073151829758963"
+          crossOrigin="anonymous"
+        ></script>
+      </Head>
       <body className="antialiased">
         <Main />
         <NextScript />
